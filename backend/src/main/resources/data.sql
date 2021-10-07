5b41715dfde97f0a72a81bb670c7077659558830
Geraldo-git <laboratorio.inicial@gmail.com>
2021-10-07 10:59:46 -0300

    Database seed

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Electronics');
+INSERT INTO tb_category (name) VALUES ('Garden');
+INSERT INTO tb_category (name) VALUES ('Dress');
+INSERT INTO tb_category (name) VALUES ('Pets');
