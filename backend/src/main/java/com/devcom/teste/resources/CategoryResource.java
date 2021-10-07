8db98839cbf20f140eca01c1b3a543b35f2fa56e
Geraldo-git <laboratorio.inicial@gmail.com>
2021-10-07 11:31:46 -0300

    DTO created

--- backend/src/main/java/com/devcom/teste/resources/CategoryResource.java
+++ backend/src/main/java/com/devcom/teste/resources/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.devcom.teste.entities.Category;
+import com.devcom.teste.dto.CategoryDTO;
 import com.devcom.teste.services.CategoryService;
 
 @RestController
@@ -19,8 +19,8 @@ public class CategoryResource {
 	CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/devcom/teste/services/CategoryService.java
+++ backend/src/main/java/com/devcom/teste/services/CategoryService.java
@@ -1,11 +1,13 @@
 package com.devcom.teste.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devcom.teste.dto.CategoryDTO;
 import com.devcom.teste.entities.Category;
 import com.devcom.teste.repositories.CategoryRepository;
 
@@ -16,8 +18,11 @@ public class CategoryService {
 	CategoryRepository categoryRepository;
 
 	@Transactional(readOnly = true)
-	public List<Category> findAll() {
-		return categoryRepository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = categoryRepository.findAll();
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
 	}
 
 }
